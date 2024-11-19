d0febecddbc4ff49a7866c68faa65ba331df53c5
luxjoshyua <josh.e.fielding@gmail.com>
2024-11-19 11:59:02 +1100

    added props

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,54 +1,55 @@
-import { RefObject } from "react"
-import { ILocomotiveScrollOptions } from "locomotive-scroll"
+import { RefObject } from 'react';
+import { ILocomotiveScrollOptions } from 'locomotive-scroll';
 
 export type ImageProps = {
-  src: string
-  alt: string
-  className?: string
-  "data-scroll"?: boolean
-  "data-scroll-speed"?: string
-  index?: number
-}
+  src: string;
+  alt: string;
+  className?: string;
+  'data-scroll'?: boolean;
+  'data-scroll-speed'?: string;
+  index?: number;
+};
 
 export type LocomotiveScrollProps = {
-  locoScrollRef: RefObject<HTMLElement>
-  children: React.ReactNode
-  el?: HTMLElement
-}
+  locoScrollRef: RefObject<HTMLElement>;
+  children: React.ReactNode;
+  el?: HTMLElement;
+};
 
 export interface IExtendedLocomotiveScrollOptions
   extends ILocomotiveScrollOptions {
-  el: HTMLElement
+  el: HTMLElement;
 }
 
 export type CustomLinkProps = {
-  href: string
-  className: string
-  children: React.ReactNode
-}
+  href: string;
+  className: string;
+  children: React.ReactNode;
+};
 
 export type ButtonProps = {
-  href: string
-  text: string
-  bgColor?: string
-  textColor?: string
-  className?: string
-}
+  href: string;
+  text: string;
+  bgColor?: string;
+  textColor?: string;
+  className?: string;
+};
 
 export type MotionImageProps = {
-  src: string
-  alt: string
-  "data-scroll"?: boolean
-  "data-scroll-speed"?: string
-  initial?: { opacity?: number; scale?: number; y?: number }
-  whileInView?: { opacity?: number; scale?: number; y?: number }
-  transition?: { duration: number; delay: number; ease: string }
-  className?: string
-}
+  src: string;
+  alt: string;
+  'data-scroll'?: boolean;
+  'data-scroll-speed'?: string;
+  initial?: { opacity?: number; scale?: number; x?: number; y?: number };
+  whileInView?: { opacity?: number; scale?: number; x?: number; y?: number };
+  transition?: { duration: number; delay: number; ease: string | number[] };
+  viewport?: { once: boolean };
+  className?: string;
+};
 
 export interface AnimateTextProps {
-  elementRef: RefObject<HTMLDivElement>
-  selector: string
-  delay: number
-  inView: boolean
+  elementRef: RefObject<HTMLDivElement>;
+  selector: string;
+  delay: number;
+  inView: boolean;
 }
