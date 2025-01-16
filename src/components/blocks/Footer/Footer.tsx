24e1d35e18cf7647df92c72b7b2c8cb91ec9a9ae
Luke Thompson <luke.thompson@nine.com.au>
2025-01-17 09:19:50 +1100

    fixed CustomLink import

--- src/components/blocks/Footer/Footer.tsx
+++ src/components/blocks/Footer/Footer.tsx
@@ -1,22 +1,23 @@
-import logo from '../../../assets/images/common/logo.svg'
-import CustomLink from '../../miscellaneous/CustomLink.tsx'
-import { BRAND_URL, BRAND_NAME } from '../../../js/global-variables.ts'
+import logo from '../../../assets/images/common/logo.svg';
+import { CustomLink } from '../../miscellaneous/index.tsx';
+import { BRAND_URL, BRAND_NAME } from '../../../js/global-variables.ts';
 
 const Footer = () => {
   return (
-    <footer className="integration-message absolute bottom-[60px] flex items-center justify-center flex-col w-full text-center z-10">
-      <h3 className="integration-text text-white font-lato text-[9px] font-medium leading-none m-0 pb-[12px] uppercase">
-        Advertising feature <span className="reader-only">{`sponsored by ${BRAND_NAME}`}</span>
+    <footer className="integration-message absolute bottom-[60px] z-10 flex w-full flex-col items-center justify-center text-center">
+      <h3 className="integration-text m-0 pb-[12px] font-lato text-[9px] font-medium uppercase leading-none text-white">
+        Advertising feature{' '}
+        <span className="reader-only">{`sponsored by ${BRAND_NAME}`}</span>
       </h3>
       <CustomLink href={BRAND_URL} className="integration-logo group">
         <img
           src={logo}
           alt={`${BRAND_NAME} logo`}
-          className="group-hover:scale-110 transition-all duration-200 ease-out"
+          className="transition-all duration-200 ease-out group-hover:scale-110"
         />
       </CustomLink>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/blocks/Header/Header.tsx
+++ src/components/blocks/Header/Header.tsx
@@ -1,27 +1,29 @@
-import { motion } from 'framer-motion'
-import logo from '../../../assets/images/common/logo.svg'
-import CustomLink from '../../miscellaneous/CustomLink.tsx'
-import { BRAND_URL, BRAND_NAME } from '../../../js/global-variables.ts'
+import { motion } from 'framer-motion';
+import logo from '../../../assets/images/common/logo.svg';
+import { CustomLink } from '../../miscellaneous/index.tsx';
+import { BRAND_URL, BRAND_NAME } from '../../../js/global-variables.ts';
 
 const Header = () => {
   return (
     <motion.header
-      className="integration-message absolute top-[60px] flex items-center justify-center flex-col w-full text-center z-10"
+      className="integration-message absolute top-[60px] z-10 flex w-full flex-col items-center justify-center text-center"
       initial={{ opacity: 0, y: '-10rem' }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.85, delay: 0.3, ease: [0.42, 0, 0.58, 1] }}>
-      <h3 className="integration-text text-white font-lato text-[9px] font-medium leading-none m-0 pb-[12px] uppercase">
-        Advertising feature <span className="reader-only">{`sponsored by ${BRAND_NAME}`}</span>
+      transition={{ duration: 0.85, delay: 0.3, ease: [0.42, 0, 0.58, 1] }}
+    >
+      <h3 className="integration-text m-0 pb-[12px] font-lato text-[9px] font-medium uppercase leading-none text-white">
+        Advertising feature{' '}
+        <span className="reader-only">{`sponsored by ${BRAND_NAME}`}</span>
       </h3>
       <CustomLink href={BRAND_URL} className="integration-logo group">
         <img
           src={logo}
           alt={`${BRAND_NAME} logo`}
-          className="group-hover:scale-110 transition-all duration-200 ease-out"
+          className="transition-all duration-200 ease-out group-hover:scale-110"
         />
       </CustomLink>
     </motion.header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
