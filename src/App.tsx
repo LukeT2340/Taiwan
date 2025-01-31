998467058952c5809fb73c3bc04dc29b3ed97d27
Luke Thompson <luke.thompson@nine.com.au>
2025-01-31 15:43:34 +1100

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from './components/blocks/Header';
 import Footer from './components/blocks/Footer';
 import Hero from './components/blocks/Hero';
 import SectionTwo from './components/blocks/SectionTwo';
-import SectionThree from './components/blocks/SectionThree';
+// import SectionThree from './components/blocks/SectionThree';
 import SmoothScrolling from './js/smooth-scrolling';
 
 const App = () => {
