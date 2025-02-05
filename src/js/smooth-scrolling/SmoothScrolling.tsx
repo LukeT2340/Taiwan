805da3361ef50eb03e9b0ef96367f48ef245d27a
Luke Thompson <luke.thompson@nine.com.au>
2025-02-06 09:44:25 +1100

    changed smooth scrolling settings

--- src/js/smooth-scrolling/SmoothScrolling.tsx
+++ src/js/smooth-scrolling/SmoothScrolling.tsx
@@ -2,7 +2,7 @@ import { ReactLenis } from '@studio-freight/react-lenis';
 
 function SmoothScrolling({ children }: { children: React.ReactNode }) {
   const lenisOptions = {
-    lerp: 0.08, // Controls how smooth the scrolling is
+    lerp: 0.04, // Controls how smooth the scrolling is
     duration: 14, // Slows down or speeds up the scrolling
     smoothTouch: false, // Disable smooth scroll on touch devices
     smooth: true, // Smooth scroll for desktop (obviously)
